636ea365e7bbb99465f891fd975592398a0a4c93
Celine Dine <cdine@cdine-macbook.lan>
2023-12-15 12:15:27 +0100

    App architecture

---
+++ swifty-companion/swifty-companion/swifty_companionApp.swift
@@ -0,0 +1,17 @@
+//
+//  swifty_companionApp.swift
+//  swifty-companion
+//
+//  Created by Celine Dine on 14/12/2023.
+//
+
+import SwiftUI
+
+@main
+struct swifty_companionApp: App {
+    var body: some Scene {
+        WindowGroup {
+            HomeView()
+        }
+    }
+}
